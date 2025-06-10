6b2759da4e46965cc11c8a4266c06d94755dbb6a
Vladimir Li <vlad@balance.ca>
2025-06-10 13:44:27 -0400

    implement stake deactivation and withdrawal parsing

--- lib/solana/program/stake.rb
+++ lib/solana/program/stake.rb
@@ -18,12 +18,21 @@ module Solana
         },
         2 => {
           instruction: :uint32
+        },
+        5 => {
+          instruction: :uint32
+        },
+        4 => {
+          instruction: :uint32,
+          amount: :uint64
         }
       }
 
       INSTRUCTION_NAMES = {
         0 => 'initialize',
-        2 => 'delegate'
+        2 => 'delegate',
+        5 => 'undelegate',
+        4 => 'withdraw'
       }
 
       PROGRAM_NAME = 'stake'
@@ -60,6 +69,31 @@ module Solana
             stake_history_sysvar: keys[3][:pubkey],
             stake_config: keys[4][:pubkey],
             stake_authority: keys[5][:pubkey],
+          }
+        when 'undelegate'
+          return {
+            from_address: keys[2][:pubkey],
+            to_address: keys[0][:pubkey],
+            program_name: PROGRAM_NAME,
+            instruction_name: INSTRUCTION_NAMES[d[:instruction]],
+
+            stake_account: keys[0][:pubkey],
+            clock_sysvar: keys[1][:pubkey],
+            stake_authority: keys[2][:pubkey],
+          }
+        when 'withdraw'
+          return {
+            from_address: keys[0][:pubkey],
+            to_address: keys[1][:pubkey],
+            program_name: PROGRAM_NAME,
+            instruction_name: INSTRUCTION_NAMES[d[:instruction]],
+
+            stake_account: keys[0][:pubkey],
+            destination: keys[1][:pubkey],
+            clock_sysvar: keys[2][:pubkey],
+            stake_history_sysvar: keys[3][:pubkey],
+            withdraw_authority: keys[4][:pubkey],
+            lamports: d[:amount]
           }
         else
           raise "Unknown instruction: #{instruction_name}"
@@ -99,6 +133,39 @@ module Solana
             { pubkey: SYSVAR_STAKE_HISTORY_ID, is_signer: false, is_writable: false }, # stake history sysvar
             { pubkey: SYSVAR_STAKE_CONFIG_ID, is_signer: false, is_writable: false }, # stake config account
             { pubkey: stake_authority, is_signer: true, is_writable: false } # stake authority
+          ]
+
+          represent(keys, data)
+        end
+
+        def undelegate_instruction(stake_account:, stake_authority:)
+          fields = INSTRUCTION_LAYOUTS[5]
+          data = encode_data(fields, {
+            instruction: 5
+          })
+
+          keys = [
+            { pubkey: stake_account, is_signer: false, is_writable: true }, # stake account
+            { pubkey: SYSVAR_CLOCK_ID, is_signer: false, is_writable: false }, # clock sysvar
+            { pubkey: stake_authority, is_signer: true, is_writable: false } # stake authority
+          ]
+
+          represent(keys, data)
+        end
+
+        def withdraw_instruction(stake_account:, destination:, withdraw_authority:, amount:)
+          fields = INSTRUCTION_LAYOUTS[4]
+          data = encode_data(fields, {
+            instruction: 4,
+            amount: amount
+          })
+
+          keys = [
+            { pubkey: stake_account, is_signer: false, is_writable: true }, # stake account
+            { pubkey: destination, is_signer: false, is_writable: true }, # destination account
+            { pubkey: SYSVAR_CLOCK_ID, is_signer: false, is_writable: false }, # clock sysvar
+            { pubkey: SYSVAR_STAKE_HISTORY_ID, is_signer: false, is_writable: false }, # stake history sysvar
+            { pubkey: withdraw_authority, is_signer: true, is_writable: false } # withdraw authority
           ]
 
           represent(keys, data)
